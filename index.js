9b7634aec5a73d4f336cace956853512cb15810f
MFHamilton <mariahamiltonr1@gmail.com>
2025-03-11 01:03:25 -0400

    CORS installed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const sql = require('mssql');
 
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Configuración de la conexión a SQL Server
 const sql = require('mssql');
@@ -20,3 +22,19 @@ const dbConfig = {
 
 }
 
+// Obtener listado de medicamentos (GET)
+app.get('/api/Medicamentos', async(req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().execute('sp_obtener_medicamentos');
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
